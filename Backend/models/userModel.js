9f913928a64cb92053d36a3076f53d685604beaa
abhaypatil11 <abhaypatil072003@gmail.com>
2025-03-01 23:22:35 +0530

    token change

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -4,8 +4,10 @@ const userSchema=new mongoose.Schema({
     name: {type:String,required:true},
     email: {type:String,unique:true,required:true},
     password: {type:String,required:true},
-    skills: {type:[String],required:true,unique:true}, 
-    location: {type:String,required:true},
+    skills: {type:[String],required:true,unique:false}, 
+    location: {type:String,required:false},
     //resumeUrl: String, 
 })
+
+
 export default mongoose.model("User",userSchema);
